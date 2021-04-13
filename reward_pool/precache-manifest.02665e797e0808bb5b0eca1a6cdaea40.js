536c0e79c6da2da0f202b86c13e35a5c6154c285
LuminaryJiao <pandaworld1985@outlook.com>
2021-04-13 20:29:25 +0800

    update reward pool

---
+++ reward_pool/precache-manifest.02665e797e0808bb5b0eca1a6cdaea40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df6dc65874d4e8efe0c184494eafc17",
+    "url": "https://astonishgong.github.io/reward_pool/index.html"
+  },
+  {
+    "revision": "63499ff8b4d34ec2d6c2",
+    "url": "https://astonishgong.github.io/reward_pool/static/js/2.c110ec99.chunk.js"
+  },
+  {
+    "revision": "c96d1ec68e847fa20691",
+    "url": "https://astonishgong.github.io/reward_pool/static/js/main.851f2832.chunk.js"
+  },
+  {
+    "revision": "c787490cc9ebd841d40e",
+    "url": "https://astonishgong.github.io/reward_pool/static/js/runtime~main.2b59f30f.js"
+  },
+  {
+    "revision": "0fcce48be414f482791aa167f4f2b294",
+    "url": "https://astonishgong.github.io/reward_pool/static/media/baseLogo.0fcce48b.png"
+  },
+  {
+    "revision": "0efc3d93b2ca7287c5f5ce7deada0170",
+    "url": "https://astonishgong.github.io/reward_pool/static/media/circle.0efc3d93.svg"
+  },
+  {
+    "revision": "67d0bb3d887c71ee2c36b498dec35a97",
+    "url": "https://astonishgong.github.io/reward_pool/static/media/down.67d0bb3d.svg"
+  },
+  {
+    "revision": "e8b8d9fc61796932cedf7caa5e54b80d",
+    "url": "https://astonishgong.github.io/reward_pool/static/media/emailIcon.e8b8d9fc.svg"
+  },
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "https://astonishgong.github.io/reward_pool/static/media/metamask.023762b6.png"
+  },
+  {
+    "revision": "64419ce113346c98bdc19bfbb9c4011c",
+    "url": "https://astonishgong.github.io/reward_pool/static/media/navbar-bg.64419ce1.png"
+  },
+  {
+    "revision": "d594fbdd04d38e369457fef381f58d3c",
+    "url": "https://astonishgong.github.io/reward_pool/static/media/twitterIcon.d594fbdd.svg"
+  },
+  {
+    "revision": "8215855c185176eb79446ce8cc1f3998",
+    "url": "https://astonishgong.github.io/reward_pool/static/media/walletconn.8215855c.svg"
+  }
+]);
